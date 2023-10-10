eb50e13191ea8e1e403fd7116c76544f550ba2f0
临江仙 <2607881950@qq.com>
2023-10-10 21:34:15 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import * as timeFormat from './timeFormat'
 import * as loaders from './loaders'
 import * as clone from './clone'
-import * as test from './test'
 
 export default {
-  ...test,
   ...clone,
   ...loaders,
   ...timeFormat
--- src/test.js
+++
@@ -1,9 +0,0 @@
-const hello = 'hello'
-const world = 'world'
-
-export function sum(a, b) {
-  return a + b
-}
-export function log() {
-  console.log(hello + world)
-};
